f0cbc727ed948c399f5c48239e8cae8ce18fa940
Ross Turk <ross@rossturk.com>
2021-06-16 22:36:51 +0000

    fix responsiveness on integration page template
    Signed-off-by: Ross Turk <ross@rossturk.com>

--- src/templates/integration.tsx
+++ src/templates/integration.tsx
@@ -3,7 +3,6 @@ import { MDXProvider } from "@mdx-js/react"
 import { graphql, PageProps } from "gatsby"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
-import { Calendar } from "react-feather"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
 import { Row, Col } from "../components/shortcodes/index"
@@ -19,14 +18,11 @@ export default function porfolio ({ location, data }: PageProps<IntegrationQuery
             }}
             location={location}
         >
-            <div className="md:px-4 mt-12 py-6 md:w-11/12 mx-auto post-content">
+            <div className="md:px-4 mt-12 py-6 md:w-11/12 mx-auto">
                 <div className="mx-auto relative">
-                    {data.mdx.frontmatter.banner.publicURL.endsWith('.svg') ?
-                    <img src={data.mdx.frontmatter.banner.publicURL} alt="''"/> :
-                    <Img fluid={data.mdx.frontmatter.banner.childImageSharp.fluid}/>}
                     <div className="flex items-center justify-center relative lg:absolute w-full h-full top-0 left-0">
                         <div className="px-4 py-8 lg:p-0 relative z-10 text-center text-color-default lg:text-white bg-bgalt lg:bg-transparent">
-                            <h1 className="int-title text-5xl font-bold text-color-2">
+                            <h1 className="text-5xl font-bold text-color-2 lg:text-white">
                                 {data.mdx.frontmatter.title}
                             </h1>
                             <p className="mt-3 md:w-3/4 mx-auto">
@@ -34,8 +30,11 @@ export default function porfolio ({ location, data }: PageProps<IntegrationQuery
                             </p>
                         </div>
                     </div>
+                    {data.mdx.frontmatter.banner.publicURL.endsWith('.svg') ?
+                    <img src={data.mdx.frontmatter.banner.publicURL} alt="''"/> :
+                    <Img fluid={data.mdx.frontmatter.banner.childImageSharp.fluid}/>}
                 </div>
-                <div className="lg:w-3/4 md:w-11/12 sm:w-full p-3 mt-4 md:mt-6 mx-auto lg:mt-12">
+                <div className="lg:w-3/4 md:w-11/12 sm:w-full p-3 mt-4 md:mt-6 mx-auto lg:mt-12 post-content">
                     <MDXProvider components={{ Row, Col }}>
                         <MDXRenderer>{data.mdx.body}</MDXRenderer>
                     </MDXProvider>
