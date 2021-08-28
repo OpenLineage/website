2b11e521e843c97c1f1093e65570a312ac8d2ff9
Ross Turk <ross@rossturk.com>
2021-08-28 01:08:13 +0000

    first pass at an integration matrix
    Signed-off-by: Ross Turk <ross@rossturk.com>

--- src/components/item-integration.tsx
+++ src/components/item-integration.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react"
-import { Button } from "./ui"
+import { Link } from "gatsby"
 import Img from "gatsby-image"
 import { ArrowRight } from "react-feather"
 
@@ -7,45 +7,46 @@ import { IndexPageQuery_integration_edges_node } from "../pages/__generated__/In
 
 type ItemIntegrationProps = { data: IndexPageQuery_integration_edges_node, even: boolean };
 export const ItemIntegration: React.FC<ItemIntegrationProps> = ({ data, even }) => {
-    return (
-        <div className="large-container mx-auto">
-            <div
-                className={`my-4 py-8 lg:py-24 integration-item md:flex
-                    ${even ? "even flex-row-reverse" : ""}`}
-            >
-                <div className="relative flex-1">
-                    <div className="relative">
-                        <Img
-                            fluid={
-                                data.frontmatter.image.childImageSharp.fluid
-                            }
-                            alt={data.frontmatter.title}
-                        />
-                    </div>
-                </div>
-                <div className="flex-1 flex md:px-4 lg:px-6 items-center">
-                    <div
-                        className={`flex flex-1 flex-wrap  ${
-                            even ? "md:justify-end md:text-right" : ""
-                        }`}
-                    >
-                        <h3 className="text-color-1 text-5xl">
-                            {data.frontmatter.title}
-                        </h3>
-                        <p className="lg:mt-4">
-                            {data.frontmatter.description}
-                        </p>
-                        <Button
-                            to={data.fields.slug}
-                            label={`Read more about ${data.frontmatter.title}`}
-                            title={"Read More"}
-                            iconRight={<ArrowRight />}
-                        />
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+
+    let datasources = data.frontmatter.datasources.split(',')
+
+    let rowList = datasources.map((datasource,index) =>{
+        return (
+            <tr className={`integration-item`}>
+                {index === 0 && (
+                    <td className="int-desc" rowspan={datasources.length}>
+                        <h3 className="mt-0"><Link to={data.fields.slug} title={"Read More"}>{data.frontmatter.title}</Link></h3>
+                        <p>{data.frontmatter.description}</p>
+                    </td>
+                )}
+  
+                {index === 0 && (
+                    <td rowspan={datasources.length}>
+                        <p>{data.frontmatter.version}</p>
+                    </td>
+                )}
+                <td>
+                    <p>{datasource}</p>
+                </td>
+                {index === 0 && (
+                    <td rowspan={datasources.length}>
+                        <ul>
+                            <li className="mb-2 text-color-2"><Link to={data.fields.slug} title={"Read More"}>Docs <ArrowRight className="inline-block" /></Link></li>
+                            {data.frontmatter.github && (
+                                <li className="mb-2 text-color-2"><Link to={data.frontmatter.github} title={"Read More"}>GitHub <ArrowRight className="inline-block" /></Link></li>
+                            )}
+
+                            {data.frontmatter.blog && (
+                                <li className="mb-2 text-color-2"><Link to={data.frontmatter.blog} title={"Read More"}>Blog <ArrowRight className="inline-block" /></Link></li>
+                            )}
+                        </ul>
+                    </td>
+                )}
+            </tr> 
+        )
+    })
+
+    return rowList
 }
 
 export default ItemIntegration;
