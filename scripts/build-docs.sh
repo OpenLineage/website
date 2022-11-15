77090dbdbf680aaecc9df2a980bf8f9992829b28
howardyoo <howardyoo@gmail.com>
2022-11-15 17:33:03 -0600

    Updates the OpenLineage.yml with latest spec version. fixes: #71
    Signed-off-by: howardyoo <howardyoo@gmail.com>

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -9,6 +9,7 @@ LATEST_VERSION=$(find . -maxdepth 1 | grep -v 'facets' | grep '[0-9]*-[0-9]-[0-9
 echo $LATEST_VERSION
 rm ./OpenLineage.json
 ln -sf "${LATEST_VERSION}/OpenLineage.json" "."
+perl -i -pe"s/version: [[:alnum:]\.-]*/version: ${LATEST_VERSION}/g" ./OpenLineage.yml
 popd
 
 yarn run redoc-cli build --output "${APIDOC_DIR}/openapi/index.html" "${SPEC_DIR}/OpenLineage.yml" --title 'OpenLineage API Docs'
